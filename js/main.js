aa0a6dda5b171ff2108e99bc8396453f1604661c
Alejandra Patino <ale@Alejandras-MacBook-Pro.local>
2020-02-11 14:07:47 -0800

    refactor JS

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 /*----- constants -----*/
 
-
+const players = {
+    player1: 1,
+    player2: 2
+}
 
 /*----- app's state (variables) -----*/
 
@@ -9,10 +12,11 @@ let turn = 1;
 let winner;
 
 
+
 /*----- cached element references -----*/
 
-const cells = Array.from(document.querySelectorAll('#board div'));
-
+const cells = document.querySelectorAll('#board > div');
+const message = document.querySelector('h2');
 /*----- event listeners -----*/
 
 document.getElementById('board').addEventListener('click', turnMade);
@@ -20,49 +24,51 @@ document.getElementById('board').addEventListener('click', turnMade);
 /*----- functions -----*/
 
 function init() {
-    board = [
-        0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,
-        0,0,0,0,0,0,0,
-    ];
+    board =  new Array(42).fill(null);
+    turn = 1;
+    winner = null;
+    selectedCell = null;
     render();
 };
- 
+init()
 
 
 function render(){
-    board.forEach(function(play, i) {
-        //console.log(play,i);   
-    cells[i].textcontent = play;
+    cells.forEach(function(e, i) {
+        //console.log(play,i)
+    let color;
+    if (board[i] === selectedCell){
+        color = 'player1';
+    };   
+    cells[i].textcontent = e;
     });
-};
+    message.textContent = `${turn}'s turn!`};
+
 
 function turnMade(event) {
     let idx = cells.findIndex(function(box) {
         return box === event.target;
     });
     board[idx] = turn;
-    //console.log(board);
-    if (turn === 1) {
+    
+        if (turn === 1) {
         turn = 2 }
         else {
         turn = 1
-        }
-    }
+        };
+    render();
+    
 
 
 
 };
 
-init();
+// init();
 
 
 
-//first player is red(1) (goes first) second player is yellow(2)(goes second)
+//player1 is red(1) (goes first) player2 is yellow(2)(goes second)
 //player whose turn it is picks a column 
-//their tile will fall on the first open spot in that column 
+//their tile will fall on the first open spot (0) in that column 
 //players keep picking until someone connects 4 
-//0 - empty cell 1- red 2- yellow
+//0 - empty cell 1- red 2- yellow;
